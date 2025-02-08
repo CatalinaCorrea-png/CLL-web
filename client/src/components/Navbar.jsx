f47e79a91c0193241c46eea14181a3aae11a6ef3
Catalina Correa <catalinayazmincorrea@gmail.com>
2025-02-08 13:55:05 -0300

    cambio de onClick por eventListener en links para cerrar sec-nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,27 +7,29 @@ import { useEffect, useState } from 'react';
 export const Navbar = () => {
   const [fabricbool, setFabricbool] = useState(false);
   const [repbool, setRepbool] = useState(false);
-  console.log(repbool);
-  console.log(fabricbool);
+  // console.log(repbool);
+  // console.log(fabricbool);
 
   const toggleFabric = () => {
     setRepbool(false);
     setFabricbool(!fabricbool);
-    console.log(fabricbool);
+    // console.log(fabricbool);
   }
 
   const toggleRep = () => {
     setFabricbool(false);
     setRepbool(!repbool);
-    console.log(repbool);
+    // console.log(repbool);
   }
   
   const closeNavs = () => {
     setFabricbool(false);
     setRepbool(false);
-    console.log(fabricbool);
-    console.log(repbool);
+    // console.log(fabricbool);
+    // console.log(repbool);
   }
+  const links = document.querySelectorAll(".react-link");
+  links.forEach((link) => link.addEventListener('click', closeNavs));
 
   return (
     <>
@@ -40,7 +42,7 @@ export const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <div className="me-auto navbar-nav"></div>
             <div className="navbar-nav">
-              <Link className="nav-link" aria-current="page" to='/'>INICIO</Link>
+              <Link className="nav-link react-link" aria-current="page" to='/'>INICIO</Link>
 
               <a className="nav-link dropdown-toggle" 
               href="#" role="button"  
@@ -56,8 +58,8 @@ export const Navbar = () => {
                 REPARACION
               </a>
 
-              <Link className="nav-link" to="/obras">OBRAS</Link>
-              <Link className="nav-link" to="/servicios">SERVICIOS</Link>
+              <Link className="nav-link react-link" to="/obras">OBRAS</Link>
+              <Link className="nav-link react-link" to="/servicios">SERVICIOS</Link>
             </div>
           </div>
           
@@ -68,14 +70,14 @@ export const Navbar = () => {
         <div className="container-fluid container-sec text-center">
           <Link className="nav-link disabled poppins-bold-italic" to="/fabricacion">FABRICACION :</Link>
           <div className="navbar-nav container-fluid row-cols-3">
-            <Link className="nav-link col link-todo" to="/fabricacion" onClick={closeNavs}>TODO</Link>
-            <Link className="nav-link col" to="/fabricacion/murales" onClick={closeNavs}>MURALES</Link>
-            <Link className="nav-link col" to="/fabricacion/bateas" onClick={closeNavs}>BATEAS</Link>
-            <Link className="nav-link col" to="/fabricacion/congelados" onClick={closeNavs}>CONGELADOS</Link>
-            <Link className="nav-link col" to="/fabricacion/exhibidoras" onClick={closeNavs}>EXHIBIDORAS</Link>
-            <Link className="nav-link col" to="/fabricacion/camaras" onClick={closeNavs}>CAMARAS Y WALK IN COOLER</Link>
-            <Link className="nav-link col" to="/fabricacion/vitrinas" onClick={closeNavs}>VITRINAS</Link>
-            <Link className="nav-link col" to="/fabricacion/usadas" onClick={closeNavs}>USADAS REACONDICIONADAS</Link>
+            <Link className="nav-link col link-todo react-link" to="/fabricacion">TODO</Link>
+            <Link className="nav-link col react-link" to="/fabricacion/murales">MURALES</Link>
+            <Link className="nav-link col react-link" to="/fabricacion/bateas">BATEAS</Link>
+            <Link className="nav-link col react-link" to="/fabricacion/congelados">CONGELADOS</Link>
+            <Link className="nav-link col react-link" to="/fabricacion/exhibidoras">EXHIBIDORAS</Link>
+            <Link className="nav-link col react-link" to="/fabricacion/camaras">CAMARAS Y WALK IN COOLER</Link>
+            <Link className="nav-link col react-link" to="/fabricacion/vitrinas">VITRINAS</Link>
+            <Link className="nav-link col react-link" to="/fabricacion/usadas">USADAS REACONDICIONADAS</Link>
           </div>
         </div>
       </div>
@@ -84,9 +86,9 @@ export const Navbar = () => {
         <div className="container-fluid container-sec text-center">
           <a className="nav-link disabled poppins-bold-italic row" href="#">REPARACION :</a>
           <div className="navbar-nav container-fluid row-cols-3">
-            <Link className="nav-link col link-todo" to="/reparacion" onClick={closeNavs} >TODO</Link>
-            <Link className="nav-link col" to="/reparacion/antesydespues" onClick={closeNavs}>ANTES Y DESPUES</Link>
-            <Link className="nav-link col" to="/reparacion/cerramientos" onClick={closeNavs}>CERRAMIENTOS CON PUERTAS DE VIDRIO</Link>
+            <Link className="nav-link col link-todo react-link" to="/reparacion">TODO</Link>
+            <Link className="nav-link col react-link" to="/reparacion/antesydespues">ANTES Y DESPUES</Link>
+            <Link className="nav-link col react-link" to="/reparacion/cerramientos">CERRAMIENTOS CON PUERTAS DE VIDRIO</Link>
           </div>
         </div>
       </div>
