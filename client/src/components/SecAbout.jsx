5ac42a7e0c7c39f87cfc7af4a94db28010f09bbb
Catalina Correa <catalinayazmincorrea@gmail.com>
2025-02-17 20:00:56 -0300

    creo seccion 'nosotros' en home

---
+++ client/src/components/SecAbout.jsx
@@ -0,0 +1,23 @@
+import '../css/home.css';
+import img from '../assets/img-nosotros.jpg';
+
+const SecAbout = () => {
+  return (
+    <>
+    <section className="seccion-about container-fluid text-center">
+      <h1 className='titulo-nosotros'>ACERCA DE NOSOTROS</h1>
+      <div className='container-nosotros'>
+        <div className='container-img'>
+          <img src={img} alt="" className='imagen-nosotros' />
+        </div>
+        <div className='texto-nosotros text-center'>
+          <p>Nuestra empresa cuenta con mas de 30 años de experiencia en instalaciones para almacenes, supermercados y comercios. Brindamos también asesoramiento respaldado por proyectos de diseño exclusivos para cada necesidad.</p>
+          <p>Nuestra mision es la de brindar soluciones integrales a las necesidades de equipamiento e instalación que nuestros clientes requieran ofreciendo un servicio de altqa calidad de producto, asesoramiento y logística.</p>    
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default SecAbout
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import CarouselFade from "../components/Carousel"
+import SecAbout from "../components/SecAbout"
 
 const Home = () => {
   return (
-    <div className="home poppins-regular">Home</div>
+    <>
+      <section className="carrusel container-fluid p-0">
+        <CarouselFade />
+      </section>
+      <SecAbout />
+      <section className="container-fluid">
+        <div>
+          Que realizamos / Servicios
+        </div>
+      </section>
+      <section className="container-fluid">
+        <div>
+        Envianos tu consulta por email o a través del formulario de contacto
+        </div>
+      </section>
+      <section className="container-fluid">
+        <div>
+          Contanto
+        </div>
+      </section>
+    </>
   )
 }
 
